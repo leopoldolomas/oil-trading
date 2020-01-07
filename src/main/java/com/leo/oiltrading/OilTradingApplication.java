e1f9d24b685fc43c70af51c4b7d6c6586e584053
Leo Lomas <leopoldolomas@gmail.com>
2020-01-07 17:09:34 -0600

    Initial upload

---
+++ src/main/java/com/leo/oiltrading/OilTradingApplication.java
@@ -0,0 +1,13 @@
+package com.leo.oiltrading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OilTradingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OilTradingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leo/oiltrading/OilTradingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leo.oiltrading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OilTradingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
